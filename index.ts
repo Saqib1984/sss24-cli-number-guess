26cec95545ec4d843fd36e6e833f48b4dee5ba61
Saqib1984 <saqibsiddiqui1984@gmail.com>
2024-03-23 19:49:41 +0500

    the number guessing game created during ramdan

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+// 1) system will generate a random number
+
+// 2) user choosing number
+
+//3) compare between user and system generated number and show result
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: chalk.yellow.bold.italic("Please guess a number between 1 t0 6 : "),
+},]);
+
+if (answer.userGuessedNumber === randomNumber){
+    console.log(chalk.green.bold.italic("Congratulation! you guessed right number.==>>", answer.userGuessedNumber))
+} else {console.log(chalk.blue.bold("Sorry! Number ==>>",answer.userGuessedNumber, "wrong answer, please try again."))
+}
+
